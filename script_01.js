46c8ec96ca7c056badbe31e694916f623951e2cc
Marcus Beringer <marcusberinger-comcave@web.de>
2022-11-15 11:43:04 +0100

    mit FOR-Schleife

--- script_01.js
+++ script_01.js
@@ -40,11 +40,11 @@
 //     output("index i: " + i);
 // }
 
-/* 02b. For-Schleife für Array-Index (Iteration)*/
-let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
-for (let i = 0; i < arr.length; i++) {
-    output("index i:" + i + " " + arr[i]); // i als Index des Arrays
-}
+// /* 02b. For-Schleife für Array-Index (Iteration)*/
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) {
+//     output("index i:" + i + " " + arr[i]); // i als Index des Arrays
+// }
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
@@ -52,40 +52,70 @@ for (let i = 0; i < arr.length; i++) {
 // output(getSentence("Ich","bin","der","coole","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
 // more of the same / DRY!
-function getSentence(word1,word2,word3,word4,word5,word6) {
-    const GAP   = " ";
-    const PUNCT = ".";
-    let str     =   word1 + GAP +
-                    word2 + GAP +
-                    word3 + GAP +
-                    word4 + GAP +
-                    word5 + GAP +
-                    word6 +
-                    PUNCT;
+// function getSentence(word1,word2,word3,word4,word5,word6) {
+//     const GAP   = " ";
+//     const PUNCT = ".";
+//     let str     =   word1 + GAP +
+//                     word2 + GAP +
+//                     word3 + GAP +
+//                     word4 + GAP +
+//                     word5 + GAP +
+//                     word6 +
+//                     PUNCT;
 
-    return str;
-}
+//     return str;
+// }
 
 /*** 01a. Funktionalität mit Array 1  */
 // Kritik | Überlegung
 // word1,word2, .... wordN :: Semantische Struktur
 // arr[0],arr[1] ... arr[n] ::Numerische Struktur (0,1,2,3 ... n)
 // --> Transformation semantisches Problem --> numerisches Problem
-// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]))
-function getSentenceArr(arr) {
-    const GAP   = " ";
-    const PUNCT = ".";
-    let str     =   arr[0] + GAP +
-                    arr[1] + GAP +
-                    arr[2] + GAP +
-                    arr[3] + GAP +
-                    arr[4] + GAP +
-                    arr[5] +
-                    PUNCT;
+// // output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]))
+// function getSentenceArr(arr) {
+//     const GAP   = " ";
+//     const PUNCT = ".";
+//     let str     =   arr[0] + GAP +
+//                     arr[1] + GAP +
+//                     arr[2] + GAP +
+//                     arr[3] + GAP +
+//                     arr[4] + GAP +
+//                     arr[5] +
+//                     PUNCT;
 
-    return str;
+//     return str;
+// }
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+
+let a = 0; // Anfangswert
+for (let i = 0; i < 10; i++) {
+    output("in der loop:" + a);
+    a = a + 1;
 }
+output("nach der loop: " + a);
 
+
+//*** 01b. Funtionalität mit Array 2 *///
+// Transponnierung : untereinander ---> nebeneinander
+output
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
 function output(outputStr) {
