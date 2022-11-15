1d750ee87fb29fdda1cd0516a1b7aa4f9a4c5a23
Marcus Beringer <marcusberinger-comcave@web.de>
2022-11-15 12:13:32 +0100

    01b. Funktionalität mit Array 2  */ // Transponierung:  untereinander

--- script_01.js
+++ script_01.js
@@ -40,58 +40,19 @@
 //     output("index i: " + i);
 // }
 
-// /* 02b. For-Schleife für Array-Index (Iteration)*/
+/* 02b. For-Schleife für Array-Index (Iteration)*/
 // let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
 // for (let i = 0; i < arr.length; i++) {
 //     output("index i:" + i + " " + arr[i]); // i als Index des Arrays
 // }
 
-/*** 01. Funktionalität mit Einzelparametern */
-// --> "Ich bin Max Mütze."
-// output(getSentence("Ich","bin","Max","Mütze"));
-// output(getSentence("Ich","bin","der","coole","Max","Mütze"));
-// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-// more of the same / DRY!
-// function getSentence(word1,word2,word3,word4,word5,word6) {
-//     const GAP   = " ";
-//     const PUNCT = ".";
-//     let str     =   word1 + GAP +
-//                     word2 + GAP +
-//                     word3 + GAP +
-//                     word4 + GAP +
-//                     word5 + GAP +
-//                     word6 +
-//                     PUNCT;
+ /*********   Überlegungen - Transponierung **********/
 
-//     return str;
-// }
-
-/*** 01a. Funktionalität mit Array 1  */
-// Kritik | Überlegung
-// word1,word2, .... wordN :: Semantische Struktur
-// arr[0],arr[1] ... arr[n] ::Numerische Struktur (0,1,2,3 ... n)
-// --> Transformation semantisches Problem --> numerisches Problem
-// // output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]))
-// function getSentenceArr(arr) {
-//     const GAP   = " ";
-//     const PUNCT = ".";
-//     let str     =   arr[0] + GAP +
-//                     arr[1] + GAP +
-//                     arr[2] + GAP +
-//                     arr[3] + GAP +
-//                     arr[4] + GAP +
-//                     arr[5] +
-//                     PUNCT;
-
-//     return str;
-// }
-/*
+/* 
 1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
 Solange die Variable existiert, bleibt dieser erhalten.
 hier: Aufsummierung
 */
-
-
 
 /* Zu "Fuß" - DRY !!! */
 // let a = 0; // Anfangswert
@@ -104,45 +65,106 @@ hier: Aufsummierung
 // output("inhalt von a: " + a);
 
 /* Besser: mit FOR-Schleife */
-
 // let a = 0; // Anfangswert
 // for (let i = 0; i < 10; i++) {
-//     output("in der loop:" + a);
-//     a = a + 1;
+//    output("in der loop:" + a);
+//    a = a + 1;
 // }
 // output("nach der loop: " + a);
 
 /*
-
 1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
-
 Solange die Variable existiert, bleibt dieser erhalten
-
 hier: Verkettung eines Strings // Transponierung
-
 */
 
+// let str = "";
+// let gap = " ";
+// let addStr = "Test";
+// for (let i = 0; i <= 5; i++) {
+//     output("in der loop: " + str);
+//     // a = a + 1;  // Numerik
+//     str = str + addStr + gap // Text
+// }
+// output("nach der loop: " + str);
 
 
-let str = "";
-let gap = " ";
-let addStr = "Test";
-for (let i = 0; i <= 10; i++) {
-    output("in der loop: " + str);
-// a = a + 1;  // Numerik
-    str = str + addStr + gap // Text
+
+
+/*** 01. Funktionalität mit Einzelparametern */
+// --> "Ich bin Max Mütze."
+// output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","der","coole","Max","Mütze"));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+// more of the same / DRY!
+function getSentence(word1,word2,word3,word4,word5,word6) {
+    const GAP   = " ";
+    const PUNCT = ".";
+    let str     =   word1 + GAP +
+                    word2 + GAP +
+                    word3 + GAP +
+                    word4 + GAP +
+                    word5 + GAP +
+                    word6 +
+                    PUNCT;
+
+    return str;
+}
+
+/*** 01a. Funktionalität mit Array 1  */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n] ::Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]))
+function getSentenceArr(arr) {
+    const GAP   = " ";
+    const PUNCT = ".";
+    let str     =   arr[0] + GAP +
+                    arr[1] + GAP +
+                    arr[2] + GAP +
+                    arr[3] + GAP +
+                    arr[4] + GAP +
+                    arr[5] +
+                    PUNCT;
+
+    return str;
+}
+
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr2(arr) {
+    const GAP   = " ";
+    const PUNCT = ".";
+    let str = "";
+
+   for (let i = 0; i < arr.length; i++) {
+
+        // wenn ich noch NICHT das letzte Element habe --> gap
+        // wenn ich das letzte Element habe -->  punct
+        // a = a + 1 || a += 1  
+
+        if (i < arr.lenght - 1) 
+        {
+            str += arr[i] + GAP;
+        } else {
+            str += arr[i] + PUNCT;
+        }  
+    }
+
+   // str = str.trim();
+    return str;//+ PUNCT;
 
 }
 
-output("nach der loop: " + str);
 
-//*** 01b. Funtionalität mit Array 2 *///
-// Transponnierung : untereinander ---> nebeneinander
-output
+
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
 function output(outputStr) {
     console.log(outputStr);
 }
-
 
